2263cc8b0c1c12b27ff16a3f7bdce9b0ce76d487
nikokosh <annikokosheva@yandex.ru>
2016-09-20 14:43:59 +0200

    autocomplete feature is added

--- controllers/wiki-controller.js
+++ controllers/wiki-controller.js
@@ -1,14 +1,46 @@
 angular.module('wikiApp')
-    .controller('WikiController', ['$scope', '$log', 'WikiFactory', function($scope, $log, wikiFactory) {
+    .controller('WikiController', ['$scope', '$log', '$window', 'WikiFactory', function($scope, $log, $window, wikiFactory) {
 
+        $scope.searchForm = {
+            keyword: ''
+        };
+        // array of retrieved articles after form submit
         $scope.wikiArticles = [];
+        $scope.articlesLoading = false;
+        $scope.autocompleteIsShown = false;
+        // use it to hide autocomlete propositions field when clicked outside
+        $scope.$window = $window;
 
-        $scope.searchWikiArticles = function(kword) {
-            $log.log(kword);
-            wikiFactory.getWikipediaArticles(kword)
+        // search an entire article
+        $scope.searchWikiArticles = function() {
+            $scope.autocompleteIsShown = false;
+            $scope.articlesLoading = true;
+            wikiFactory.getWikipediaArticles($scope.searchForm.keyword)
                 .then(function(data) {
                     $scope.wikiArticles = data.data.query.pages;
-                    $log.log($scope.wikiArticles);
-                })
-        } 
+                    $scope.articlesLoading = false;
+                });
+        }
+
+        // search for article titles to use them in autocomplete
+        $scope.searchTitles = function(keyword) {
+            // filtering input: if input.length is bigger then 3 and only after 1.5 seconds
+            if(keyword.length >= 3) {
+                setTimeout(function() {
+                    wikiFactory.getWikipediaTitles(keyword)
+                        .then(function(data) {
+                            // $log.log(data)
+                            $scope.wikiTitles = data.data.query.pages;
+                            $scope.autocompleteIsShown = true;
+                        });
+                }, 1500);
+            }
+        }
+
+        // use autocomplete proposition as #keyword input value
+        $scope.showAutocompleteTitles = function(word) {
+            $scope.searchForm.keyword = word;
+            $scope.autocompleteIsShown = false;
+        }
+     
     }]);
--- factories/wiki-factory.js
+++ factories/wiki-factory.js
@@ -1,10 +1,11 @@
 angular.module('wikiApp')
     .factory('WikiFactory', ['$http', function($http) {
 
+        var wikiApiStart = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+        var wikiApiEnd = '&callback=JSON_CALLBACK';
+
         function getWikipediaArticles(keyword) {
 
-            var wikiApiStart = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
-            var wikiApiEnd = '&callback=JSON_CALLBACK';
 
             return $http.jsonp(wikiApiStart + keyword + wikiApiEnd)
                 .success(function(response) {
@@ -12,7 +13,16 @@ angular.module('wikiApp')
                 });
         }
 
+        function getWikipediaTitles(keyword) {
+            // intitle added in order to search by titles only
+            return $http.jsonp(wikiApiStart + 'intitle:' + keyword + wikiApiEnd)
+                .success(function(response) {
+                    return response;
+                })
+        }
+
         return {
-            getWikipediaArticles: getWikipediaArticles
+            getWikipediaArticles: getWikipediaArticles,
+            getWikipediaTitles: getWikipediaTitles
         }
     }]);
